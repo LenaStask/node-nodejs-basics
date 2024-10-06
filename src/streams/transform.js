a65c2daf5ab32560f19c9ffe8c920d765a0c0fd0
lenaStsk <staskevichalena@gmail.com>
2024-10-06 16:58:11 +0200

    add: implement streams functions

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,14 @@
+import { Transform } from "stream";
+import { pipeline } from "stream/promises";
+
 const transform = async () => {
-    // Write your code here 
+  const ts = new Transform({
+    transform(chunk, encoding, callback) {
+      callback(null, chunk.toString().split("").reverse().join(""));
+    },
+  });
+
+  await pipeline(process.stdin, ts, process.stdout);
 };
 
 await transform();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { pipeline } from "node:stream/promises";
+
 const write = async () => {
-    // Write your code here 
+  const __fileName = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__fileName);
+  const filePath = join(__dirname, "files", "fileToWrite.txt");
+
+  const ws = fs.createWriteStream(filePath, { flags: "a" });
+
+  await pipeline(process.stdin, ws);
 };
 
 await write();
