eea395536c50752d4d0870af75a9d1c3c705755c
lenaStsk <staskevichalena@gmail.com>
2024-10-06 13:30:45 +0200

    add: implement parsArgs function

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,17 @@
+import { argv } from "node:process";
+
 const parseArgs = () => {
-    // Write your code here 
+  const result = argv
+    .slice(2)
+    .reduce((total, element, index, array) => {
+      if (element.startsWith("--")) {
+        const changedElement = `${element.slice(2)} is ${array[index + 1]}`;
+        total.push(changedElement);
+      }
+      return total;
+    }, [])
+    .join(", ");
+  console.log(result);
 };
 
 parseArgs();
