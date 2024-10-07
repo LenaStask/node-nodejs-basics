7103f5b244103082550147e8b06305fa23f35810
lenaStsk <staskevichalena@gmail.com>
2024-10-07 11:54:18 +0200

    feat: implement calculateHash function

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import crypto from "node:crypto";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+import { createReadStream } from "node:fs";
+
 const calculateHash = async () => {
-    // Write your code here 
+  const __fileName = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__fileName);
+  const filePath = join(__dirname, "files", "fileToCalculateHashFor.txt");
+
+  const rs = createReadStream(filePath);
+
+  const hash = crypto.createHash("SHA256");
+
+  rs.pipe(hash).on("finish", () => console.log(hash.digest("hex")));
 };
 
 await calculateHash();
