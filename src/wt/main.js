a8a29a0b41bf135a059396d8a45aa17324b5ae34
lenaStsk <staskevichalena@gmail.com>
2024-10-07 22:00:51 +0200

    feat: implement worker threads

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,31 @@
-const performCalculations = async () => {
-    // Write your code here
-};
+import { Worker } from "worker_threads";
+import { fileURLToPath } from "url";
+import { dirname, join, resolve } from "path";
+import { cpus } from "os";
 
+const performCalculations = async () => {
+  const __fileName = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__fileName);
+  const filePath = join(__dirname, "worker.js");
+
+  const numCores = cpus().length;
+  const workers = [];
+
+  const createWorker = (workerData) =>
+    new Promise((resolve, reject) => {
+      const worker = new Worker(filePath, { workerData });
+
+      worker.on("message", (data) =>
+        resolve({ status: "resolved", data: data })
+      );
+      worker.on("error", () => resolve({ status: "error", data: null }));
+    });
+
+  for (let i = 0; i < numCores; i++) {
+    workers.push(createWorker(10 + i));
+  }
+
+  const result = await Promise.all(workers);
+  console.log(result);
+};
 await performCalculations();
